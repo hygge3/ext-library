a6baf51c20bead8d21bce7ba24099a3800fb6ca8
zhanglh <mrzhanglh@foxmail.com>
2025-01-16 13:39:42 +0800

    refactor: 重构密码加密功能
    - 使用 spring-security-crypto 来代替自己实现的加密功能
    - 加密功能移动到 core 模块

---
+++ ext-core/src/main/java/ext/library/core/util/Encryptor.java
@@ -0,0 +1,106 @@
+package ext.library.core.util;
+
+import lombok.experimental.UtilityClass;
+import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
+
+/**
+ * 加密器
+ */
+@UtilityClass
+public class Encryptor {
+    PasswordEncoder DEFAULT_ENCODER = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+
+    private final static Argon2PasswordEncoder ARGON2_ENCODER = Argon2PasswordEncoder.defaultsForSpringSecurity_v5_8();
+    private final static BCryptPasswordEncoder BCRYPT_ENCODER = new BCryptPasswordEncoder();
+    private final static Pbkdf2PasswordEncoder PBKDF2_ENCODER = Pbkdf2PasswordEncoder.defaultsForSpringSecurity_v5_8();
+
+    /**
+     * 通过 默认加密器 BCrypt 加密
+     *
+     * @param plaintext 纯文本
+     * @return {@code String }
+     */
+    public String encrypt(String plaintext) {
+        return DEFAULT_ENCODER.encode(plaintext);
+    }
+
+    /**
+     * 通过 默认加密器 BCrypt 检查
+     *
+     * @param plaintext      纯文本
+     * @param passwordHashed 密码散列
+     * @return boolean
+     */
+    public boolean check(String plaintext, String passwordHashed) {
+        return DEFAULT_ENCODER.matches(plaintext, passwordHashed);
+    }
+
+    /**
+     * 通过 Argon2 加密
+     *
+     * @param plaintext 纯文本
+     * @return {@code String }
+     */
+    public String encryptByArgon2(String plaintext) {
+        return ARGON2_ENCODER.encode(plaintext);
+    }
+
+    /**
+     * 通过 Argon2 检查
+     *
+     * @param plaintext      纯文本
+     * @param passwordHashed 密码散列
+     * @return boolean
+     */
+    public boolean checkByArgon2(String plaintext, String passwordHashed) {
+        return ARGON2_ENCODER.matches(plaintext, passwordHashed);
+    }
+
+
+    /**
+     * 通过 BCrypt 加密
+     *
+     * @param plaintext 纯文本
+     * @return {@code String }
+     */
+    public String encryptByBCrypt(String plaintext) {
+        return BCRYPT_ENCODER.encode(plaintext);
+    }
+
+    /**
+     * 通过 BCrypt 检查
+     *
+     * @param plaintext      纯文本
+     * @param passwordHashed 密码散列
+     * @return boolean
+     */
+    public boolean checkByBCrypt(String plaintext, String passwordHashed) {
+        return BCRYPT_ENCODER.matches(plaintext, passwordHashed);
+    }
+
+    /**
+     * 通过 PBKDF2 加密
+     *
+     * @param plaintext 纯文本
+     * @return {@code String }
+     */
+    public String encryptByPBKDF2(String plaintext) {
+        return PBKDF2_ENCODER.encode(plaintext);
+    }
+
+    /**
+     * 通过 PBKDF2 检查
+     *
+     * @param plaintext      纯文本
+     * @param passwordHashed 密码散列
+     * @return boolean
+     */
+    public boolean checkByPBKDF2(String plaintext, String passwordHashed) {
+        return PBKDF2_ENCODER.matches(plaintext, passwordHashed);
+    }
+
+}
