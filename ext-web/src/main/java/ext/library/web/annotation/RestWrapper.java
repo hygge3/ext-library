15fb532167f682042e71e7a564ffa7dbebd410d9
zlh <zhanglh30@gmail.com>
2025-09-04 16:20:07 +0800

    refactor(ext-web): 优化

--- ext-web/src/main/java/ext/library/web/annotation/RestWrapper.java
+++ ext-web/src/main/java/ext/library/web/annotation/RestWrapper.java
@@ -1,12 +1,13 @@
 package ext.library.web.annotation;
 
+import org.springframework.core.annotation.AliasFor;
+
 import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-import org.springframework.core.annotation.AliasFor;
 
 /**
  * REST 响应包装器
